93de2922bcf8db975ccc7e002c6099b5c914d211
Daniel Chen <yumpty@Daniels-MacBook-Air-5.local>
2023-10-08 23:29:07 -0400

    does not create a temp image

--- video.py
+++ video.py
@@ -1,8 +1,10 @@
 import urllib.request
 import eyed3
 import os
-from moviepy.editor import *
+os.environ["IMAGEIO_FFMPEG_EXE"] = "/opt/homebrew/bin/ffmpeg"
 from eyed3.id3.frames import ImageFrame
+from moviepy.editor import *
+
 
 
 class Video:
@@ -19,10 +21,7 @@ class Video:
   def download(self):
     self.stream.download(output_path = self.output_path, filename = self.filename, filename_prefix = self.filename_prefix, skip_existing = self.skip_existing)    
     
-    # following code from https://stackoverflow.com/questions/65668113/how-to-set-thumbnail-for-mp3-using-eyed3-python-module
-    
-    urllib.request.urlretrieve(self.thumbnail_url, "tmp.jpg")
-    
+    # following code from https://stackoverflow.com/questions/65668113/how-to-set-thumbnail-for-mp3-using-eyed3-python-module    
     mp4filepath = self.output_path + '/' + self.filename
     mp3filepath = self.output_path + '/' + self.title + '.mp3'
 
@@ -39,12 +38,13 @@ class Video:
     audiofile.tag.title = self.title
     audiofile.tag.author = self.author
 
-    audiofile.tag.images.set(ImageFrame.FRONT_COVER, open("tmp.jpg", 'rb').read(), 'image/jpeg')
+    response = urllib.request.urlopen(self.thumbnail_url)
+    imagedata = response.read()
+    audiofile.tag.images.set(ImageFrame.FRONT_COVER, imagedata, 'image/jpeg')
 
     audiofile.tag.save(version=eyed3.id3.ID3_V2_3)
 
     video.close()
     
-    os.remove('tmp.jpg')
     os.remove(mp4filepath)
     
